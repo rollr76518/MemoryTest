2d11cd1607cd8cb5c135e87add827fc1236cebd8
Hanyu.Chen <hanyu.chen@nexiosoft.com>
2019-06-12 13:19:05 +0800

    Initial Commit

---
+++ MemoryTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryTest
+//
+//  Created by Hanyu on 2019/6/12.
+//  Copyright © 2019 Hanyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
